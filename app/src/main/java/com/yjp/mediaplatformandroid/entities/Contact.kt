6be9f42218a4ab345fda708a84809c9eef7feec0
yjp <yjp19871013@126.com>
2018-01-02 16:25:57 +0800

    Rename Contact to RemoteContact.

--- app/src/main/java/com/yjp/mediaplatformandroid/entities/Contact.kt
+++
@@ -1,9 +0,0 @@
-package com.yjp.mediaplatformandroid.entities
-
-import com.google.gson.annotations.SerializedName
-
-
-data class Contact(val id: String,
-                   @SerializedName(value = "user_id") val userId: String,
-                   val name: String,
-                   @SerializedName(value = "phone_number") val phoneNumber: String)
---
+++ app/src/main/java/com/yjp/mediaplatformandroid/entities/RemoteContact.kt
@@ -0,0 +1,9 @@
+package com.yjp.mediaplatformandroid.entities
+
+import com.google.gson.annotations.SerializedName
+
+
+data class RemoteContact(val id: String,
+                         @SerializedName(value = "user_id") val userId: String,
+                         val name: String,
+                         @SerializedName(value = "phone_number") val phoneNumber: String)
