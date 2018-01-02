cc160647017dfc7d2793c0a9f3edf2b335448273
yjp <yjp19871013@126.com>
2018-01-02 17:09:48 +0800

    Rename.

--- app/src/main/java/com/yjp/mediaplatformandroid/activities/RemoteContactsActivity.kt
+++ app/src/main/java/com/yjp/mediaplatformandroid/activities/RemoteContactsActivity.kt
@@ -8,6 +8,6 @@ class RemoteContactsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contacts)
+        setContentView(R.layout.activity_listview)
     }
 }
