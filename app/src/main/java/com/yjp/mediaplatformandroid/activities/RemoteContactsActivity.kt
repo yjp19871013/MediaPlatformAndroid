4276cb9975008fd0ab0858b26fa0ae15b6d6c925
yjp <yjp19871013@126.com>
2018-01-04 11:08:26 +0800

    Remove unusable import.

--- app/src/main/java/com/yjp/mediaplatformandroid/activities/RemoteContactsActivity.kt
+++ app/src/main/java/com/yjp/mediaplatformandroid/activities/RemoteContactsActivity.kt
@@ -1,6 +1,5 @@
 package com.yjp.mediaplatformandroid.activities
 
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
